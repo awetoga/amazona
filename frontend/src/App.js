a9350abce26e13e62d547a8a86c5338ed62f86f9
Awetoga Abraham <awetogaabraham1@gmail.com>
2023-04-10 11:07:09 +0000

    Video-03-create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <a href= "/">amazona</a>
+      </header>
+      <main>
+        list Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
